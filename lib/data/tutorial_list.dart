e2a0765f6102d1464695973f980e4b2347be85d0
Gauri24982003 <kulkarnigauri2006@gmail.com>
2024-03-08 08:41:12 +0530

    Addition of tutorials

--- lib/data/tutorial_list.dart
+++ lib/data/tutorial_list.dart
@@ -13,5 +13,42 @@ const List<TutorialModel> tutorialList = [
     duration: "7 min",
     fileName: "while_loop.md",
   ),
+  TutorialModel(
+    title: "If Else",
+    description: "If-Else Statement in Dart",
+    duration: "5 min",
+    fileName: "if_else.md",
+  ),
+  TutorialModel(
+    title: "If Else-If",
+    description: "If Else-If Statement in Dart",
+    duration: "8 min",
+    fileName: "if_else_if.md",
+  ),
+  TutorialModel(
+    title: "Functions",
+    description: "Functions in Dart",
+    duration: "10 min",
+    fileName: "function.md",
+  ),
+  TutorialModel(
+    title: "Classes And Objects",
+    description: "Object Oriented Programming",
+    duration: "15 min",
+    fileName: "classes_objects.md",
+  ),
+  TutorialModel(
+    title: "Constructors",
+    description: "Object Oriented Programming",
+    duration: "7 min",
+    fileName: "constructor.md",
+  ),
+  TutorialModel(
+    title: "Try-Catch",
+    description: "Exception Handling",
+    duration: "10 min",
+    fileName: "try_catch.md",
+  ),
+  
   
 ];
