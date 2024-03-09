a315d94b9d09e8e880c9531abd3f844a289fa803
Dhirenvinay <127668442+Dhirenvinay@users.noreply.github.com>
2024-03-09 13:34:29 +0530

    Update quiz_screen.dart

--- lib/ui/activity/quiz_screen.dart
+++ lib/ui/activity/quiz_screen.dart
@@ -1,3 +1,4 @@
+import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_dart/provider/quiz_provider.dart';
@@ -23,33 +24,33 @@ class _QuizScreenState extends State<QuizScreen> {
       body: Column(
         children: [
           Expanded(
-            child: Swiper(
+            child: AppinioSwiper(
+              cardCount: quizProvider.questions.length,
               controller: quizProvider.swiperController,
-              loop: false,
-              itemBuilder: (BuildContext context, int index) {
-                return buildQuestion(quizProvider.questions[index]);
+              onSwipeEnd: (previousIndex, targetIndex, activity) {
+                print(
+                    "the index ${targetIndex} and ${previousIndex} and ${activity}");
+                quizProvider.mainIndex = previousIndex;
+                // print("the position ${position.index} ");
+                // quizProvider.updateMainIndex(position.index);
+                quizProvider.checkAnswer(previousIndex);
               },
-              itemCount: quizProvider.questions.length,
-              viewportFraction: 0.8,
-              scale: 0.9,
-              onIndexChanged: (index) {
-                quizProvider.updateMainIndex(index);
-
-                // setState(() {
-                // quizProvider.mainIndex = index;
-                // quizProvider.notifyListeners();
-                // });
+              onCardPositionChanged: (position) {},
+              cardBuilder: (BuildContext context, int index) {
+                return buildQuestion(quizProvider.questions[index]);
               },
             ),
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(20, 10, 20, 50),
+            padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ElevatedButton(
-                  onPressed: quizProvider.isBack()
+                  onPressed: quizProvider.mainIndex > 0
                       ? () {
+                          print("the index ${quizProvider.mainIndex > 0}");
+                          // quizProvider.swiperController.unswipe();
                           quizProvider.goBack();
                         }
                       : null,
@@ -57,7 +58,8 @@ class _QuizScreenState extends State<QuizScreen> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    quizProvider.checkAnswer();
+                    // quizProvider.checkAnswer(quizProvider.mainIndex);
+                    quizProvider.swiperController.swipeRight();
                   },
                   child: const Text("Next"),
                 ),
@@ -73,42 +75,45 @@ class _QuizScreenState extends State<QuizScreen> {
     QuizProvider quizProvider = Provider.of<QuizProvider>(context);
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          question['question'],
-          style: const TextStyle(fontSize: 20),
-        ),
-        const SizedBox(height: 20),
-        Column(
-          children: List.generate(
-            question['options'].length,
-            (index) => Padding(
-              padding: EdgeInsets.all(
-                  (question['options'][index] == quizProvider.selectedAnswer)
-                      ? 0
-                      : 8.0),
-              child: RadioListTile(
-                title: Text(question['options'][index]),
+    return Card(
+      // color: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            question['question'],
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          Column(
+            children: List.generate(
+              question['options'].length,
+              (index) => Padding(
+                padding: EdgeInsets.all(
+                    (question['options'][index] == quizProvider.selectedAnswer)
+                        ? 0
+                        : 8.0),
+                child: RadioListTile(
+                  title: Text(question['options'][index]),
 
-                tileColor:
-                    question['options'][index] == quizProvider.selectedAnswer
-                        ? colorScheme.primaryContainer
-                        : colorScheme.secondaryContainer,
-                // activeColor: colorScheme.error,
-                // fillColor: colorScheme.onError,
-                value: question['options'][index],
-                groupValue: quizProvider.selectedAnswer,
-                onChanged: (value) {
-                  quizProvider.updateSelectedAnswer(value);
-                },
+                  tileColor:
+                      question['options'][index] == quizProvider.selectedAnswer
+                          ? colorScheme.primaryContainer
+                          : colorScheme.secondaryContainer,
+                  // activeColor: colorScheme.error,
+                  // fillColor: colorScheme.onError,
+                  value: question['options'][index],
+                  groupValue: quizProvider.selectedAnswer,
+                  onChanged: (value) {
+                    quizProvider.updateSelectedAnswer(value);
+                  },
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
